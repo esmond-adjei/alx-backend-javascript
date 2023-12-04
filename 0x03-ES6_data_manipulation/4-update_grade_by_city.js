2a295fe0772cd43e7a425c3a34576672f50901ad
esmond-adjei <xmongram@gmail.com>
2023-12-04 18:57:33 +0000

    new file:   0x03-ES6_data_manipulation/4-main.js 	new file:   0x03-ES6_data_manipulation/4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const newGrade = newGrades.filter((grade) => grade.studentId === student.id)[0];
+      return {
+        ...student,
+        grade: newGrade ? newGrade.grade : 'N/A',
+      };
+    });
+}
