49dfd3b2316f25672bf68be7e8356049d1bb31df
esmond-adjei <xmongram@gmail.com>
2023-12-04 19:30:07 +0000

    new file:   0x03-ES6_data_manipulation/100-main.js 	new file:   0x03-ES6_data_manipulation/100-weak.js

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,10 @@
+export const weakMap = new WeakMap();
+
+export const queryAPI = (endpoint) => {
+    let called = weakMap.get(endpoint) || 0;
+    called += 1;
+    weakMap.set(endpoint, called);
+    if (called >= 5) {
+        throw new Error('Endpoint load is high');
+    }
+}
