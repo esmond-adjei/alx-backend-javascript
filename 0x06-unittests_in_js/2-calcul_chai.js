9c61faca8609533f82208e6e03aa461d04de4e1c
esmond-adjei <xmongram@gmail.com>
2024-02-01 19:34:32 +0000

    task-2

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,12 @@
+function calculateNumber(type, a, b) {
+  let result;
+  switch (type) {
+    case 'SUM': result = Math.round(a) + Math.round(b); break;
+    case 'SUBTRACT': result = Math.round(a) - Math.round(b); break;
+    case 'DIVIDE': result = Math.round(b) !== 0 ? (Math.round(a) / Math.round(b)) : 'Error'; break;
+    default: return 'Unknown type';
+  }
+  return result;
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,26 @@
+const { expect } = require("chai");
+const calculateNumber = require("./1-calcul");
+
+describe("calculateNumber", () => {
+  it("should round and divide inputs, given the right type", () => {
+    const result = calculateNumber('SUM', 6.3, 2.5); // 9
+    expect(result).to.equal(9);
+
+    const result1 = calculateNumber('SUBTRACT', 6.3, 2.5); // 3
+    expect(result1).to.equal(3);
+
+    const result2 = calculateNumber('DIVIDE', 6.3, 2.5); // 2
+    expect(result2).to.equal(2);
+
+    const result3 = calculateNumber('DIVIDE', 5.2, -2.5); // -2.5
+    expect(result3).to.equal(-2.5);
+  });
+
+  it("should take care of divide by zero", () => {
+    expect(calculateNumber('DIVIDE', 4.0, 0.123)).to.equal('Error');
+  });
+
+  it("should handle invalid types", () => {
+    expect(calculateNumber('INVALID', 3.0, 2.0)).to.equal('Unknown type');
+  });
+});
