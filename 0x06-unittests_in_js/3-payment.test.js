de1e8986f3fc2dccf90cfce1087e35fc5abeff53
esmond-adjei <xmongram@gmail.com>
2024-02-01 21:03:09 +0000

    modified:   0x06-unittests_in_js/3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,14 +1,15 @@
-const { expect } = require('chai');
 const sinon = require('sinon');
 const Utils = require('./utils');
+const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-    it('should check if sendPaymentRequestToApi uses calculateNumber method of Utils', () => {
-        const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-        sendPaymentRequestToApi(100, 20);
-        expect(calculateNumberSpy.callCount).to.be.equal(1);
-        expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
-        calculateNumberSpy.restore();
-    })
-})
+  it('should check if sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const utils = sinon.spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(utils.calculateNumber.callCount).to.be.equal(1);
+    utils.calculateNumber.restore();
+  });
+});
