57e7442cb80634178df9fe9fd6cca4983a71c31b
esmond-adjei <xmongram@gmail.com>
2024-02-01 21:07:09 +0000

    modified:   0x06-unittests_in_js/3-payment.js

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,7 +1,7 @@
-const { calculateNumber } = require('./utils');
+const Utils= require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    const total = calculateNumber('SUM', totalAmount, totalShipping);
+    const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
     console.log(`The total is: ${total}`);
 }
 
