f97dbc28312f2c453e140f5cc6d9476de6afa62b
esmond-adjei <xmongram@gmail.com>
2024-02-02 09:10:23 +0000

    upd-3:task-2, task-3

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,22 +3,22 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.toString().split('\n').filter(line => line.trim() !== '');
+    const lines = data.toString().split('\n').filter((line) => line.trim() !== '');
 
     let grpCount1 = 0;
     let grpCount2 = 0;
-    let grpList1 = [];
-    let grpList2 = [];
+    const grpList1 = [];
+    const grpList2 = [];
 
-    lines.forEach(line => {
+    lines.forEach((line) => {
       const [firstname, lastname, age, field] = line.toString().split(',');
 
       if (firstname && lastname && age && field) {
         if (field === 'CS') {
-          grpCount1++;
+          grpCount1 += 1;
           grpList1.push(firstname);
         } else if (field === 'SWE') {
-          grpCount2++;
+          grpCount2 += 1;
           grpList2.push(firstname);
         }
       }
